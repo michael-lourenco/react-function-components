fcddf22ef745dda5ed87e63271dc8ac93958a282
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-10 16:17:45 -0300

    UseState

--- react-function-components/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ react-function-components/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,34 +1,76 @@
-import React from 'react';
-import { Button, FormControlLabel, Switch, TextField } from '@material-ui/core';
+import React, { useState } from "react";
+import { Button, FormControlLabel, Switch, TextField } from "@material-ui/core";
 
 function FormularioCadastro() {
-    let nome  = "";
+    const [nome, setNome] = useState("Michael");
+
     return (
-        <form onSubmit = {event => {
+        <form 
+            onSubmit = {event => {
                 event.preventDefault();
-                console.log(nome)
+                console.log(nome);
             }
         }>
 
             <TextField 
+                value = { nome }
                 onChange = {event => {
-                    nome = event.target.value;
+                    setNome(event.target.value);
+                    if(nome.length > 3) {
+                        setNome(nome.substring(0, 3))
+                    }
                 }}
-                label = "Nome" variant = "outlined" id = "nome" margin = 'dense' fullWidth />
+                label = "Nome" 
+                variant = "outlined" 
+                id = "nome" 
+                margin = "dense" 
+                fullWidth 
+            />
 
-            <TextField label = "Sobrenome" variant = "outlined" id = "sobrenome" margin = 'dense' fullWidth />
+            <TextField 
+                label = "Sobrenome" 
+                variant = "outlined" 
+                id = "sobrenome" 
+                margin = "dense" 
+                fullWidth 
+            />
 
-            <TextField label = "CPF" variant = "outlined" id = "cpf" margin = 'dense' fullWidth />
+            <TextField 
+                label = "CPF" 
+                variant = "outlined" 
+                id = "cpf" 
+                margin = "dense" 
+                fullWidth 
+            />
 
-            <FormControlLabel control = { <Switch name = "Promoções" id = "promocoes" color = "primary" defaultChecked /> } label = "Promoções" />
+            <FormControlLabel 
+                control = { 
+                    <Switch 
+                        name = "Promoções" 
+                        id = "promocoes" 
+                        color = "primary" 
+                        defaultChecked /> 
+                    } 
+                label = "Promoções" 
+            />
 
-            <FormControlLabel control = {<Switch name = "Novidades" id = "novidades" color = "primary" defaultChecked /> } label = "Novidades" />
+            <FormControlLabel 
+                control = {
+                    <Switch 
+                        name = "Novidades" 
+                        id = "novidades" 
+                        color = "primary" 
+                        defaultChecked /> 
+                    } 
+                label = "Novidades" 
+            />
 
             <Button 
                 type = "submit"
                 variant = "contained"
                 color = "primary"
             >Cadastrar</Button>
+
         </form>
     );
 }
