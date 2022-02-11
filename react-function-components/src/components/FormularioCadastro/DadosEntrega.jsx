641027548690b1b737b7e0fb939c7acefa05ac69
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-11 09:55:37 -0300

    Criando formulários

---
+++ react-function-components/src/components/FormularioCadastro/DadosEntrega.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Button, TextField } from '@material-ui/core';
+
+function DadosEntrega(){
+    return(
+        <form>
+            <TextField  
+                type = "number"
+                label = "cep" 
+                variant = "outlined" 
+                id = "cep" 
+                margin = "dense" 
+            />
+
+            <TextField  
+                type = "text"
+                label = "Endereço" 
+                variant = "outlined" 
+                id = "endereco" 
+                margin = "dense" 
+                fullWidth 
+            />
+
+            <TextField  
+                type = "number"
+                label = "Número" 
+                variant = "outlined" 
+                id = "numero" 
+                margin = "dense" 
+            />
+            
+            <TextField  
+                type = "text"
+                label = "Estado" 
+                variant = "outlined" 
+                id = "estado" 
+                margin = "dense" 
+            />
+
+            <TextField  
+                type = "text"
+                label = "Cidade" 
+                variant = "outlined" 
+                id = "cidade" 
+                margin = "dense" 
+            />
+
+            <Button 
+                type = "submit" 
+                color = "primary"
+                variant = "contained"
+                fullWidth
+            >Finalizar Cadastro</Button>
+        </form>
+    )
+}
+
+export default DadosEntrega;
