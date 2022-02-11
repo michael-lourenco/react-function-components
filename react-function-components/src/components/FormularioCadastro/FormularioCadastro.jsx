faf717bb1f01aa449c0b2b0b604a2965df864075
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-11 10:59:49 -0300

    Renderização condicional

--- react-function-components/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ react-function-components/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
+import { Typography } from "@material-ui/core";
 import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
+    const [etapaAtual, setEtapaAtual] = useState(0);
+
+    function formularioAtual(etapaAtual) {
+        switch(etapaAtual){
+            case 0:
+                return <DadosUsuario />;
+            case 1:
+                return <DadosPessoais aoEnviar = { aoEnviar } validarCPF = { validarCPF }/>;
+            case 2:
+                return <DadosEntrega />;
+            default:
+                return <Typography>Erro ao selecionar formulário</Typography>
+        }
+    }
 
     return (
-        <>        
-            <DadosPessoais aoEnviar = { aoEnviar } validarCPF = { validarCPF }/>
-            <DadosUsuario />
-            <DadosEntrega />
+        <>  
+            { formularioAtual(etapaAtual) }           
         </>
     );
 }
