a5d130f0c5be2967ea2947416a54c8f89337e26b
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-11 13:14:30 -0300

    useEffect

--- react-function-components/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ react-function-components/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,16 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
     const [etapaAtual, setEtapaAtual] = useState(0);
+    const [dadosColetados, setDados] = useState({});
+
+    useEffect(() => {
+        console.log(dadosColetados);
+    })
 
     const formularios = [
-        <DadosUsuario aoEnviar={ proximo }/>,
-        <DadosPessoais aoEnviar = { proximo } validarCPF = { validarCPF }/>,
-        <DadosEntrega aoEnviar = { aoEnviar } />
+        <DadosUsuario aoEnviar={ coletarDados }/>,
+        <DadosPessoais aoEnviar = { coletarDados } validarCPF = { validarCPF }/>,
+        <DadosEntrega aoEnviar = { coletarDados } />
     ]
+
+    function coletarDados(dados) {
+        setDados({ ...dadosColetados, ...dados });
+        proximo();
+    }
 
     function proximo(){
         setEtapaAtual(etapaAtual + 1);
